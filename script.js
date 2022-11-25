5c3368c00e2c5ee124cce8cf199d363951ab694c
Caleb Tseng <calebtseng07@gmail.com>
2022-11-25 04:19:45 -0800

    create functions to store value and calculate inputs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let firstNum = "";
-let secondNum = "";
-let operatorChoice = undefined;
-let clearScreen = false;
+let previousValue = '';
+let currentValue = '';
+let operator = '';
 
 //query selectors
 //screen display
@@ -9,42 +8,62 @@ const previous = document.querySelector("#previous");
 const current = document.querySelector("#current");
 
 //buttons
-const numberBtns = document.querySelectorAll("#number");
-const operatorBtns = document.querySelectorAll("#operator");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector("#clearBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
 const decimalBtn = document.querySelector("#decimalBtn");
 const equalBtn = document.querySelector("#equalBtn");
 
 
-numberBtns.forEach((button) =>
-    button.addEventListener('click', () =>
-        appendNumber(button.textContent))
-);
 
-operatorBtns.forEach((button) =>
-    button.addEventListener('click', () =>
-        appendNumber(button.textContent)
+numberBtns.forEach((number) =>
+    number.addEventListener('click', function (e) {
+        numSelection(e.target.textContent);
+        current.textContent = currentValue;
+    }));
 
-        /* store current number into first operand;
-        then 
-        if add is pressed
-            then change operator choice to add
-            etc
-        clear current number 
-        update current number display
+operatorBtns.forEach((op) =>
+    op.addEventListener('click', function (e) {
+        operatorSelection(e.target.textContent)
+        previous.textContent = previousValue + ' ' + operator;
+        current.textContent = currentValue;
+    }));
 
-        */
-    )
-);
+equalBtn.addEventListener('click', function () {
+    if (currentValue != '' && previousValue != '') {
+        operate()
+        previous.textContent = '';
+    } if (current.textContent = previousValue);
+    console.log(equalBtn);
+});
 
+
+function numSelection(num) {
+    currentValue += num
+};
+
+function operatorSelection(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+};
+
+decimalBtn.addEventListener('click', addDecimal);
+
+// decimalBtn.addEventListener('click', function () {
+//     addDecimal();
+// });
+
+clearBtn.addEventListener('click', clearDisplay);
 
 function clearDisplay() {
+    currentValue = '';
+    previousValue = '';
+    operator = '';
     current.innerHTML = "";
     previous.innerHTML = "";
 };
-
-clearBtn.addEventListener('click', clearDisplay);
 
 function backSpace() {
     current.innerText = current.innerText.slice(0, -1);
@@ -52,37 +71,94 @@ function backSpace() {
 
 deleteBtn.addEventListener('click', backSpace);
 
-function appendNumber(number) {
-    if (current.textContent === '0' || clearScreen)
-        updateDisplay();
-    current.textContent += number
+// if (equalBtn == true) {
+// } else {
+//     operator == "";
+// }
+
+function operate() {
+    previousValue = Number(previousValue); //used this to convert to a number
+    currentValue = Number(currentValue); //used this to convert to a number
+
+    if (operator === "+") {
+        previousValue += currentValue
+        // add();
+    } else if (operator === "-") {
+        previousValue -= currentValue
+        // substract();
+    } else if (operator === "x") {
+        previousValue *= currentValue;
+        // multiply();
+    } else {
+        previousValue /= currentValue;
+        //     // divide()
+        // } else {
+        //     return null
+    }
+    // previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();  //changing back into string
+    currentValue = previousValue.toString();  //changing back into string
 };
 
-function updateDisplay() {
-    current.innerText = '';
-    clearScreen = false;
+function roundNumber(num) {
+    return Math.random(num * 1000) / 1000;
+};
+
+function addDecimal() {
+    if (current.textContent.includes('.')) return
+    currentValue.textContent += '.';
 }
 
+// function addDecimal() {
+//     if (!currentValue.includes('.')) {
+//         currentValue += '.';
+//     }
+// }
 
-//operator functions
-function add(a, b) {
-    return a + b
-};
 
-function substract(a, b) {
-    return a - b
-};
 
-function multiply(a, b) {
-    return a * b
-};
+// function operate(operator, a, b) {
+//     a = Number(a);
+//     b = Number(b);
+//     switch (operator) {
+//         case "+":
+//             return add(a, b);
+//         case "-":
+//             return substract(a, b);
+//         case "×":
+//             return multiply(a, b);
+//         case "÷":
+//             return divide(a, b);
+//         default:
+//             return null
 
-function divide(a, b) {
-    if (b == 0) {
-        return "-_-";
-    } else
-        return a / b
-};
+//     }
+// }
+// if (equalBtn == true) {
+// } else {
+//     operator == "";
+// }
+
+
+// // //operator functions
+// function add(a, b) {
+//     return a + b
+// };
+
+// function substract(a, b) {
+//     return a - b
+// };
+
+// function multiply(a, b) {
+//     return a * b
+// };
+
+// function divide(a, b) {
+//     if (b == 0) {
+//         return "-_-";
+//     } else
+//         return a / b
+// };
 
 // decimalBtn.addEventListener('click', numChoice)
 
@@ -90,3 +166,14 @@ function divide(a, b) {
 //     if (number === '.' && current.textContent.includes('.')) return
 //     current = current.toString() + number.toString()
 // }
+
+
+/* store current number into first operand;
+then
+if add is pressed
+    then change operator choice to add
+    etc
+clear current number
+update current number display
+
+*/
