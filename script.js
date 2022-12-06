eb1157ae8dfe58678b31f59d0c3a4aabdb3473f8
Caleb Tseng <calebtseng07@gmail.com>
2022-12-06 12:35:42 -0800

    add main code

--- script.js
+++ script.js
@@ -1,185 +1,6 @@
-// let previousValue = '';
-// let currentValue = '';
-// let operator = null;
-
-// /////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////
-// //query selectors//
-// //screen display
-// const previous = document.querySelector("#previous");
-// const current = document.querySelector("#current");
-
-// //buttons
-// const numberBtns = document.querySelectorAll(".number");
-// const operatorBtns = document.querySelectorAll(".operator");
-// const clearBtn = document.querySelector("#clearBtn");
-// const deleteBtn = document.querySelector("#deleteBtn");
-// const decimalBtn = document.querySelector("#decimalBtn");
-// const equalBtn = document.querySelector("#equalBtn");
-
-// /////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////
-// //button choices//
-
-// numberBtns.forEach((number) =>
-//     number.addEventListener('click', (e) => {
-//         numSelection(e.target.textContent);
-//         current.textContent = currentValue;
-//     }));
-
-// function numSelection(num) {
-//     if (previousValue !== '' && currentValue !== "" && operator === "") {
-//         previousValue = "";
-//         current.textContent = currentValue;
-//     }
-//     if (currentValue.length <= 11) {
-//         currentValue += num;
-//         current.textContent = currentValue;
-//     };
-// };
-
-
-// operatorBtns.forEach((op) => {
-//     op.addEventListener('click', (e) => {
-//         handleOperator(e.target.textContent)
-//         previous.textContent = previousValue + ' ' + operator;
-//         // current.textContent = currentValue;
-//     });
-// });
-
-// function handleOperator(op) {
-//     if (previousValue === '') {
-//         previousValue = currentValue;
-//         operatorSelection(op);
-//     } else {
-//         operate();
-//         operator = op;
-//         current.textContent = '0';
-//         previous.textContent = previousValue + ' ' + operator;
-//     }
-// }
-
-// function operatorSelection(test) {
-//     operator = test;
-//     previous.textContent = previousValue + ' ' + operator;
-//     current.textContent = '0';
-//     currentValue = '';
-
-// }
-// equalBtn.addEventListener('click', () => {
-//     if (currentValue !== '' && previousValue !== '') {
-//         operate()
-//         // console.log(current);
-//     }
-//         // previous.textContent = `${previousValue} ${operator} ${currentValue} =`;
-//     // } if
-//     //     (current.textContent = previousValue);
-//     // operator = null
-
-// });
-
-// // /////////////////////////////////////////////////////////////////////
-// // /////////////////////////////////////////////////////////////////////
-// // //functionality//
-
-// function operate() {
-//     previousValue = Number(previousValue); //used this to convert to a number
-//     currentValue = Number(currentValue); //used this to convert to a number
-
-//     if (operator === "+") {
-//         previousValue += currentValue
-//     } else if (operator === "-") {
-//         previousValue -= currentValue
-//     } else if (operator === "x") {
-//         previousValue *= currentValue;
-//     } else if (operator === "÷") {
-//         if (currentValue <= 0) {
-//             previousValue = "Error";
-
-//             return;
-//         }
-//         previousValue /= currentValue;
-//     }
-
-//     currentValue = roundNumber(previousValue);
-//     previousValue = previousValue.toString();  //changing back into string
-//     currentValue = previousValue.toString();  //changing back into string
-//     displayResults();
-// };
-
-// function roundNumber(num) {
-//     return Math.random(num * 100000) / 100000;
-// };
-
-// function displayResults() {
-//     if (previousValue.length <= 11) {
-//         current.textContent = previousValue // allows equal button to stop after one click
-
-//     } else {
-
-//         current.textContent = previousValue.slice(0, 11) + "...";
-//     }
-//     previous.textContent = '';
-//     // operator = '';
-//     currentValue = ''; //executes the value after pressing equal
-// };
-
-// /////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////
-// //clear and delete//
-
-// function clearDisplay() {
-//     currentValue = '';
-//     previousValue = '';
-//     operator = null;
-//     current.innerHTML = "";
-//     previous.innerHTML = "";
-// };
-
-// clearBtn.addEventListener('click', clearDisplay);
-
-// function backSpace() {
-//     current.innerText = current.innerText.slice(0, -1);
-// };
-
-// deleteBtn.addEventListener('click', backSpace);
-
-// /////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////
-// decimalBtn.addEventListener('click', () => {
-//     if (current.textContent.includes('.')) {
-//         return;
-
-//     } else current.textContent += '.';
-// });
-
-// decimalBtn.addEventListener('click', addDecimal);
-
-// function addDecimal() {
-//     if (!currentValue.includes('.')) {
-//         currentValue += '.';
-//  current.textContent = currentValue;
-//     };
-// };
-
-/* 1. have equal button stop after one click
-    2. fix bug with decimal code and stop after one click
-    3. figure out how to chain operators together
-    4. find a way to have current value to continue or delete after equal button is pushed // equation sticks on new entry after equal button
-/*
-
-/* store current number into first operand;
-then
-if add is pressed
-    then change operator choice to add
-    etc
-clear current number
-update current number display
-
-*/
 let previousValue = '';
 let currentValue = '';
-let operator = '';
+let operator = null;
 
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
@@ -207,34 +28,59 @@ numberBtns.forEach((number) =>
     }));
 
 function numSelection(num) {
-    currentValue += num
-};
-
-
-operatorBtns.forEach((op) =>
-    op.addEventListener('click', (e) => {
-        operatorSelection(e.target.textContent)
-        previous.textContent = previousValue + ' ' + operator;
+    if (previousValue !== '' && currentValue !== "" && operator === "") {
+        previousValue = "";
         current.textContent = currentValue;
-    }));
-
-function operatorSelection(op) {
-    operator = op;
-    previousValue = currentValue;
-    currentValue = '';
+    }
+    if (currentValue.length <= 11) {
+        currentValue += num;
+        current.textContent = currentValue;
+    };
 };
 
-equalBtn.addEventListener('click', () => {
-    if (currentValue != '' && previousValue != '') {
-        operate()
-    }
-    //     previous.textContent = '';
-    // } if (current.textContent = previousValue);
+
+operatorBtns.forEach((op) => {
+    op.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent)
+        previous.textContent = previousValue + ' ' + operator;
+        // current.textContent = currentValue;
+    });
 });
 
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-//functionality//
+function handleOperator(op) {
+    if (previousValue === '') {
+        previousValue = currentValue;
+        operatorSelection(op);
+    } else {
+        operate();
+        operator = op;
+        current.textContent = '0';
+        previous.textContent = previousValue + ' ' + operator;
+    }
+}
+
+function operatorSelection(test) {
+    operator = test;
+    previous.textContent = previousValue + ' ' + operator;
+    current.textContent = '0';
+    currentValue = '';
+
+}
+equalBtn.addEventListener('click', () => {
+    if (currentValue !== '' && previousValue !== '') {
+        operate()
+        // console.log(current);
+    }
+    // previous.textContent = `${previousValue} ${operator} ${currentValue} =`;
+    // } if
+    //     (current.textContent = previousValue);
+    // operator = null
+
+});
+
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //functionality//
 
 function operate() {
     previousValue = Number(previousValue); //used this to convert to a number
@@ -246,13 +92,23 @@ function operate() {
         previousValue -= currentValue
     } else if (operator === "x") {
         previousValue *= currentValue;
-    } else {
+    } else if (operator === "÷") {
+        if (currentValue <= 0) {
+            previousValue = "Error";
+
+            return;
+        }
         previousValue /= currentValue;
     }
+
     currentValue = roundNumber(previousValue);
     previousValue = previousValue.toString();  //changing back into string
     currentValue = previousValue.toString();  //changing back into string
     displayResults();
+};
+
+function roundNumber(num) {
+    return Math.random(num * 100000) / 100000;
 };
 
 function displayResults() {
@@ -268,41 +124,185 @@ function displayResults() {
     currentValue = ''; //executes the value after pressing equal
 };
 
-function roundNumber(num) {
-    return Math.random(num * 1000) / 1000;
-};
-
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 //clear and delete//
+
 function clearDisplay() {
     currentValue = '';
     previousValue = '';
-    operator = '';
+    operator = null;
     current.innerHTML = "";
     previous.innerHTML = "";
 };
 
 clearBtn.addEventListener('click', clearDisplay);
 
-
 function backSpace() {
     current.innerText = current.innerText.slice(0, -1);
 };
 
 deleteBtn.addEventListener('click', backSpace);
+
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
+decimalBtn.addEventListener('click', () => {
+    if (current.textContent.includes('.')) {
+        return;
+
+    } else current.textContent += '.';
+});
+
+decimalBtn.addEventListener('click', addDecimal);
+
 function addDecimal() {
-    if (!currentValue.includes(".")) {
+    if (!currentValue.includes('.')) {
         currentValue += '.';
         current.textContent = currentValue;
     };
 };
 
-decimalBtn.addEventListener('click', () => {
-    addDecimal();
-});
+/* 1. have equal button stop after one click
+    2. fix bug with decimal code and stop after one click
+    3. figure out how to chain operators together
+    4. find a way to have current value to continue or delete after equal button is pushed // equation sticks on new entry after equal button
+/*
+
+/* store current number into first operand;
+then
+if add is pressed
+    then change operator choice to add
+    etc
+clear current number
+update current number display
+
+// */
+// let previousValue = '';
+// let currentValue = '';
+// let operator = '';
+
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //query selectors//
+// //screen display
+// const previous = document.querySelector("#previous");
+// const current = document.querySelector("#current");
+
+// //buttons
+// const numberBtns = document.querySelectorAll(".number");
+// const operatorBtns = document.querySelectorAll(".operator");
+// const clearBtn = document.querySelector("#clearBtn");
+// const deleteBtn = document.querySelector("#deleteBtn");
+// const decimalBtn = document.querySelector("#decimalBtn");
+// const equalBtn = document.querySelector("#equalBtn");
+
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //button choices//
+
+// numberBtns.forEach((number) =>
+//     number.addEventListener('click', (e) => {
+//         numSelection(e.target.textContent);
+//         current.textContent = currentValue;
+//     }));
+
+// function numSelection(num) {
+//     currentValue += num
+// };
+
+
+// operatorBtns.forEach((op) =>
+//     op.addEventListener('click', (e) => {
+//         operatorSelection(e.target.textContent)
+//         previous.textContent = previousValue + ' ' + operator;
+//         current.textContent = currentValue;
+//     }));
+
+// function operatorSelection(op) {
+//     operator = op;
+//     previousValue = currentValue;
+//     currentValue = '';
+// };
+
+// equalBtn.addEventListener('click', () => {
+//     if (currentValue != '' && previousValue != '') {
+//         operate()
+//     }
+//     //     previous.textContent = '';
+//     // } if (current.textContent = previousValue);
+// });
+
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //functionality//
+
+// function operate() {
+//     previousValue = Number(previousValue); //used this to convert to a number
+//     currentValue = Number(currentValue); //used this to convert to a number
+
+//     if (operator === "+") {
+//         previousValue += currentValue
+//     } else if (operator === "-") {
+//         previousValue -= currentValue
+//     } else if (operator === "x") {
+//         previousValue *= currentValue;
+//     } else {
+//         previousValue /= currentValue;
+//     }
+//     currentValue = roundNumber(previousValue);
+//     previousValue = previousValue.toString();  //changing back into string
+//     currentValue = previousValue.toString();  //changing back into string
+//     displayResults();
+// };
+
+// function displayResults() {
+//     if (previousValue.length <= 11) {
+//         current.textContent = previousValue // allows equal button to stop after one click
+
+//     } else {
+
+//         current.textContent = previousValue.slice(0, 11) + "...";
+//     }
+//     previous.textContent = '';
+//     // operator = '';
+//     currentValue = ''; //executes the value after pressing equal
+// };
+
+// function roundNumber(num) {
+//     return Math.random(num * 1000) / 1000;
+// };
+
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //clear and delete//
+// function clearDisplay() {
+//     currentValue = '';
+//     previousValue = '';
+//     operator = '';
+//     current.innerHTML = "";
+//     previous.innerHTML = "";
+// };
+
+// clearBtn.addEventListener('click', clearDisplay);
+
+
+// function backSpace() {
+//     current.innerText = current.innerText.slice(0, -1);
+// };
+
+// deleteBtn.addEventListener('click', backSpace);
+// /////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// function addDecimal() {
+//     if (!currentValue.includes(".")) {
+//         currentValue += '.';
+//         current.textContent = currentValue;
+//     };
+// };
+
+// decimalBtn.addEventListener('click', () => {
+//     addDecimal();
+// });
 
 /* 1. have equal button stop after one click
 ////    2. fix bug with decimal code and stop after one click //////
